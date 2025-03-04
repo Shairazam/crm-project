c4bccc4a9feb5bcae62f5e129b9e6800d79dbceb
Shairazam <shair.yasini@gmail.com>
2025-03-04 19:22:30 +0500

    tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import './index.css' // Import Tailwind CSS here
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Users from './pages/Users';
---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./index.html",                // If you are using an index.html in the root
+      "./src/**/*.{js,jsx,ts,tsx}",  // This will look for all JS/JSX/TS/TSX files inside the src folder
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
